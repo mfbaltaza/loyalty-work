b6e8d212f43aad856a4f6dd0632f44076388abb3
Juan Araujo <araujogreydiego@gmail.com>
2023-01-20 18:37:13 -0400

    feat: basic responive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,8 @@ const Home: NextPage = () => {
         <p className="mx-auto mb-12 md:ml-32">Electronics</p>
       </div>
       <main className="mx-auto max-w-7xl">
-        <div className="catalog-controls text-xl">
-          <div className="flex items-center">
+        <div className="catalog-controls text-xl px-8 md:px-0">
+          <div className="flex flex-col md:flex-row items-center">
             <p className="product-count">16 of 32 products</p>
             <Separator.Root
               className="SeparatorRoot h-8"
@@ -35,18 +35,77 @@ const Home: NextPage = () => {
               orientation="vertical"
               style={{ margin: "0 15px" }}
             />
-            <p className="mr-6 text-gray-400">Sort by:</p>
-            <div className="badges flex gap-6">
+            <p className="mr-6 mb-6 md:mb-0 text-gray-400">Sort by:</p>
+            <div className="badges flex flex-col md:flex-row gap-6 mb-6 md:mb-0">
               <ControlBadge text="Most recent" enabled={true} />
               <ControlBadge text="Lowest price" />
-              <ControlBadge text="Highest price"/>
+              <ControlBadge text="Highest price" />
             </div>
-            <Image className="ml-auto" src="/icons/arrow-right.svg" height={48} width={48} alt="Arrow" />
+            <Image
+              className="mx-auto md:ml-auto"
+              src="/icons/arrow-right.svg"
+              height={48}
+              width={48}
+              alt="Arrow"
+            />
           </div>
           <Separator.Root
             className="SeparatorRoot"
             style={{ margin: "24px 0" }}
           />
+        </div>
+        <div className="product-showcase flex flex-col items-center gap-8 pb-8 md:flex-row">
+          <div className="product-card h-64 w-64 items-end bg-white shadow-md">
+            <div className="product-details mx-6 my-4">
+              <Image
+                className="mx-auto"
+                src="/images/product-pics/iPhone8-x1.png"
+                width={175}
+                height={175}
+                alt="Product"
+              />
+              <Separator.Root
+                className="SeparatorRoot"
+                style={{ margin: "24px 0" }}
+              />
+              <p className="text-gray-400">Laptops</p>
+              <p>Macbook Pro</p>
+            </div>
+          </div>
+          <div className="product-card h-64 w-64 items-end bg-white shadow-md">
+            <div className="product-details mx-6 my-4">
+              <Image
+                className="mx-auto"
+                src="/images/product-pics/iPhone8-x1.png"
+                width={175}
+                height={175}
+                alt="Product"
+              />
+              <Separator.Root
+                className="SeparatorRoot"
+                style={{ margin: "24px 0" }}
+              />
+              <p className="text-gray-400">Laptops</p>
+              <p>Macbook Pro</p>
+            </div>
+          </div>
+          <div className="product-card h-64 w-64 items-end bg-white shadow-md">
+            <div className="product-details mx-6 my-4">
+              <Image
+                className="mx-auto"
+                src="/images/product-pics/iPhone8-x1.png"
+                width={175}
+                height={175}
+                alt="Product"
+              />
+              <Separator.Root
+                className="SeparatorRoot"
+                style={{ margin: "24px 0" }}
+              />
+              <p className="text-gray-400">Laptops</p>
+              <p>Macbook Pro</p>
+            </div>
+          </div>
         </div>
       </main>
     </>
